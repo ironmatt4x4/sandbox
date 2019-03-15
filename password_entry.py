43a65a24b3848b55a0861bd68ca6b14eb867afef
Mattt <mattrc4x4@gmail.com>
2019-03-15 12:34:57 +1000

    Add Password checker

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,11 @@
 '''Mattt'''
+
+MIN_LENGTH = 6
+password = input('Enter a password with 6 or more characters')
+
+password_length = len(password)
+if password_length < MIN_LENGTH:
+    print('Error: password too short')
+else:
+    for i in range(0, password_length, 1):
+        print('*', end='')
